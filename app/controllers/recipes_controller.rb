1040237af59c0f392875cb303badddca20136c1e
Nicholas Powers <njpowers7915@gmail.com>
2018-07-05 15:57:36 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,8 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    @recipe.ingredients.build().save
-    @recipe.ingredients.build().save
+    @recipe.ingredients.build()
+    @recipe.ingredients.build()
   end
 
   def create
